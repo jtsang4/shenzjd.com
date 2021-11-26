2c6c29ac150724c14c138e915ee5644dbb525a18
wu529778790 <529778790@qq.com>
2021-11-26 10:56:33 +0800

    增加离线去水印

---
+++ watermark/precache-manifest.5a2686114c6c79aadd87a90d406af58f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4edd4df7dcb221efc2e186e2f31ad54",
+    "url": "/shenzjd/watermark/index.html"
+  },
+  {
+    "revision": "04c34988dcb22ffa6b46",
+    "url": "/shenzjd/watermark/static/css/main.0853bf32.chunk.css"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/shenzjd/watermark/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "04c34988dcb22ffa6b46",
+    "url": "/shenzjd/watermark/static/js/main.753e17ad.chunk.js"
+  },
+  {
+    "revision": "edd9d841f205c5de2d85",
+    "url": "/shenzjd/watermark/static/js/runtime~main.f2123258.js"
+  }
+]);
---
+++ watermark/precache-manifest.873eccc1fc639852116ecd84f6cb461d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa8774b103e5674ea5df900d4924661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce4da881f9b1a709118",
+    "url": "/static/css/main.4f52dd50.chunk.css"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "8ce4da881f9b1a709118",
+    "url": "/static/js/main.8067116b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);

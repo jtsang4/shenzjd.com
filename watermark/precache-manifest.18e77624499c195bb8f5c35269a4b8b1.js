9aaa5342a95f2746eb761911b8e6a2b8537aa849
wtzeng1 <wtzeng1@gmail.com>
2021-12-17 16:15:05 +0800

    chore: make watermarker localized

--- watermark/precache-manifest.18e77624499c195bb8f5c35269a4b8b1.js
+++ watermark/precache-manifest.18e77624499c195bb8f5c35269a4b8b1.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15aa4dec38ef167649b66825cd75a6a1",
-    "url": "/shenzjd/watermark/index.html"
+    revision: '15aa4dec38ef167649b66825cd75a6a1',
+    url: '/index.html',
   },
   {
-    "revision": "7810bb6b61c1b70972e2",
-    "url": "/shenzjd/watermark/static/css/main.4f52dd50.chunk.css"
+    revision: '7810bb6b61c1b70972e2',
+    url: '/static/css/main.4f52dd50.chunk.css',
   },
   {
-    "revision": "b6aafa07ca16cd706e06",
-    "url": "/shenzjd/watermark/static/js/2.2752fb64.chunk.js"
+    revision: 'b6aafa07ca16cd706e06',
+    url: '/static/js/2.2752fb64.chunk.js',
   },
   {
-    "revision": "7810bb6b61c1b70972e2",
-    "url": "/shenzjd/watermark/static/js/main.3f757300.chunk.js"
+    revision: '7810bb6b61c1b70972e2',
+    url: '/static/js/main.3f757300.chunk.js',
   },
   {
-    "revision": "edd9d841f205c5de2d85",
-    "url": "/shenzjd/watermark/static/js/runtime~main.f2123258.js"
-  }
+    revision: 'edd9d841f205c5de2d85',
+    url: '/static/js/runtime~main.f2123258.js',
+  },
 ]);
